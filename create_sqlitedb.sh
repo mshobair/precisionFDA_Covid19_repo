f2e42a82e3c93c0201cfcffb342a8ca1340c6a78
Mahmoud Shobair <37299523+mshobair@users.noreply.github.com>
2021-01-29 23:47:37 -0500

    Add files via upload

---
+++ create_sqlitedb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# create sqlite database instance
+sqlite3 ./test.db << EOF
+# import flat files(tsv) as tables in test.db
+.mode tabs ;
+.import $1 seqtable  
+.import $2 metadata  
+
+# create indices for seqtable on sample_processing_id and jun_aa_len column
+CREATE INDEX seq_ind ON seqtable(sample_processing_id);
+CREATE INDEX jun_aa_len ON seqtable(junction_aa_length);
+
+# create index for metadata table
+CREATE INDEX meta_ind ON metadata(sample_processing_id,subject_id, sex, disease_diagnosis, disease_stage, intervention);
+
+EOF
